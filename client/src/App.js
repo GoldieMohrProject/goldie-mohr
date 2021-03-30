8f1042d52561b14e87e5bae45c9d489f4f915810
Said Mg <saidmghabghab@gmail.com>
2021-03-30 00:50:26 -0400

    Home Page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,23 @@
 
-<<<<<<< HEAD
 import About from "./Pages/About"
 import React from 'react'
 import Wrapper from './Components/Wrapper'
 import Contact from "./Pages/Contact"
 import Footer from './Components/Footer'
-=======
-// import About from "./Pages/About"
-// import Contact from "./Pages/Contact"
 import Service from "./Pages/Services"
->>>>>>> branch-Fatih
-
-
+import Navbar from "./Components/Navbar"
+import Home from "./Pages/Home"
 
 
 function App() {
   return (
-<<<<<<< HEAD
 
     <Wrapper>
-      <h1>GOLDIE MOHR</h1>
+     < Navbar/>
+      {/* <h1>GOLDIE MOHR</h1> */}
+      <Home/>
       <Footer />
     </Wrapper>
-
-
-
-=======
-    <>
-       <Service/>
-    </>
->>>>>>> branch-Fatih
   );
 }
 
