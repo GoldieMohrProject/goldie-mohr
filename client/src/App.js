2a92340406d3117e6bb465e5a07079efcecd43c5
Fatih Sultan Ay <f.sultanay@gmail.com>
2021-03-31 14:15:00 -0400

    Revert "Update App.js"
    This reverts commit 4bbe78b6fb3a02a222e67ab4fcf8bb4c1a1ddbbd.

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ function App() {
         <Route exact path="/About" component={About} />
         <Route exact path="/" component={Home} />
         <Route exact path="/Service" component={Service} />
-        <Route exact path="/Career" component={Career} />
+        <Route exact path="/Career" component={Home} />
         <Route exact path="/Projects" component={Home} />
         <Route exact path="/Login" component={Login} />
         </Wrapper>
