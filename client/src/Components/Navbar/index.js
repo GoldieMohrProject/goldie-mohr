0851750699dff129505f1c9c8eed0ec1882610d1
mischadab <74383200+mischadab@users.noreply.github.com>
2021-04-01 14:37:50 -0400

    added link to training in navbar

--- client/src/Components/Navbar/index.js
+++ client/src/Components/Navbar/index.js
@@ -19,7 +19,7 @@ function Navbar() {
         <div class="dropdown">
         <button class="dropbtn"><i class="fas fa-user fa-lg" ></i></button>
         <div class="dropdown-content">
-          <a href="#">Training</a>
+          <a href="./Training">Training</a>
           <a href="#">Profile</a>
           <a href="#">Logout</a>
         </div>
