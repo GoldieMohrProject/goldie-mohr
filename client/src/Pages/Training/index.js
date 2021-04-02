32fd51ef9a7a40c4d0227a53d87c0afd12b1b87c
mischadab <74383200+mischadab@users.noreply.github.com>
2021-04-02 17:19:10 -0400

    i forget what i did here

--- client/src/Pages/Training/index.js
+++ client/src/Pages/Training/index.js
@@ -6,7 +6,7 @@ import Safety from '../../assets/Safety.pdf'
 function Training() {
     return (
         <div>
-
+     
             <div className="trainingarea">
                 <h1 className="trainingtitle">Training</h1>
                 <p>Please review the topics below</p>
@@ -21,8 +21,8 @@ function Training() {
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/4bkr5lpKGUM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 <p>After watching the video, please click <a href="https://www.mysafetysign.com/free-crane-safety-quiz" target="_blank">here</a> to take the test</p>
             </div>
-
-        </div>
+            </div>
+     
     )
 }
 
