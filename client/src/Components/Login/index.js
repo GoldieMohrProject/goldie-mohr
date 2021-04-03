4e1e7714dede6ae6e85d3245bf888fe729e829ce
mischadab <74383200+mischadab@users.noreply.github.com>
2021-04-03 14:30:10 -0400

    new modal

--- client/src/Components/Login/index.js
+++ client/src/Components/Login/index.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css'
+import { Button } from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
+
+
 
 function Login() {
-    <div>
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        {/* insert stuff here */}
-      </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <Button variant="primary" onClick={handleShow}>
+                Launch demo modal
+      </Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Login</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+          </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+          </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+
 }
+// render(<Login />);
+
+
+export default Login;
