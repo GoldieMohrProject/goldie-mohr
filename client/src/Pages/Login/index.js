a7c7258e454e3737ce8263e1aa29b0c4a2aa88a8
mischadab <74383200+mischadab@users.noreply.github.com>
2021-04-03 14:55:14 -0400

    idk what i did here

--- client/src/Pages/Login/index.js
+++ client/src/Pages/Login/index.js
@@ -4,22 +4,22 @@ import './style.css'
 function Login() {
     return (
         <div>
-              <div className="sup">
-            <div className="loginarea">
-            <h3 className="logintitle">Login</h3>
-            <br></br>
-            <form>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <div className="sup">
+                <div className="loginarea">
+                    <h3 className="logintitle">Login</h3>
+                    <br></br>
+                    <form>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" className="form-label">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Password</label>
+                            <input type="password" className="form-control" id="exampleInputPassword1" />
+                        </div>
+                        <button type="submit" id="loginbtn">Login</button>
+                    </form>
                 </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" />
-                </div>
-                <button type="submit" id="loginbtn">Login</button>
-            </form>
-</div>
             </div>
             <p className="qs">Haven't registered yet? Click <a href="./Register">here</a> to register</p>
         </div>
