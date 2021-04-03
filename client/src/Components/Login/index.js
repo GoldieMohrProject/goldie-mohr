a128b2aff1e892d98d445d80f50ba543f7a0bef3
mischadab <74383200+mischadab@users.noreply.github.com>
2021-04-03 14:54:44 -0400

    added form to modal

--- client/src/Components/Login/index.js
+++ client/src/Components/Login/index.js
@@ -10,27 +10,40 @@ function Login() {
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    // const handleShow = () => setShow(true);
 
     return (
         <>
-            <Button variant="primary" onClick={handleShow}>
+            {/* <Button variant="primary" onClick={handleShow}>
                 Launch demo modal
-      </Button>
+      </Button> */}
 
-            <Modal show={show} onHide={handleClose}>
+            <Modal show={true} onHide={handleClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>Login</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
+
+                <Modal.Body>
+                    <form>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" className="form-label">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Password</label>
+                            <input type="password" className="form-control" id="exampleInputPassword1" />
+                        </div>
+                        </form>
+                </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button onClick={handleClose} dialogClassName="closebtn">
+                            Cancel
           </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
+                        <Button  onClick={handleClose} dialogClassName="loginbtn">
+                            Login
           </Button>
-                </Modal.Footer>
+                    </Modal.Footer>
             </Modal>
         </>
     );
