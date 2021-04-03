6f6b7cd741a50c7b584c08f86ec4db3f31c4d3f9
mischadab <74383200+mischadab@users.noreply.github.com>
2021-04-03 16:27:59 -0400

    added modal to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import About from "./Pages/About"
-import React from 'react'
+import React, { useState } from 'react'
 import Wrapper from './Components/Wrapper'
 import Contact from "./Pages/Contact"
 import Footer from './Components/Footer'
@@ -11,14 +11,49 @@ import Login from "./Components/Login/index"
 import Career from "./Pages/Career"
 import Product from "./Pages/Products/index"
 import Training from "./Pages/Training/index"
-
+import {Modal} from "react-bootstrap"
+import {Button} from "react-bootstrap"
 
 function App() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  
   return (
     <Router>
       <div>
-        <Navbar />
+        <Navbar handleModal={handleShow}/>
         <Wrapper>
+        <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Login</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    <form>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" className="form-label">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Password</label>
+                            <input type="password" className="form-control" id="exampleInputPassword1" />
+                        </div>
+                        </form>
+                </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose} >
+                            Cancel
+          </Button>
+                        <Button variant="primary" onClick={handleClose} >
+                            Login
+          </Button>
+ 
+                    </Modal.Footer>
+                    <p style={{ textAlign: "center" }}>Haven't registered yet? Click <a href="./Register">here</a> to register</p>
+            </Modal>
         <Route exact path="/Contact" component={Contact} />
         <Route exact path="/About" component={About} />
         <Route exact path="/" component={Home} />
