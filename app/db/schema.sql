d254ba510f024836650f2d2c4437b81c4f815bad
Said Mg <saidmghabghab@gmail.com>
2021-04-04 20:51:27 -0400

    edited the schema in mysql

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -6,11 +6,13 @@ use goldie_mohr;
 create table user(
 
 userID INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+isAdmin boolean default 0,
+authId boolean default 0,
 email varchar(30),
-password varchar (30),
+password varchar (100),
 first_name varchar (30),
 last_name varchar (30),
-phone_number INTEGER (20)
+phone_number varchar (15),
+picture varchar (20)
 
 );
-
